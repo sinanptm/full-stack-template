d3ca0fc92c64082a8c27f220f6d07079b7df3b44
muhammed sinan <muhammedsinan7749@gmail.com>
2025-05-30 23:04:42 +0530

    next thems removed

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,7 +1,6 @@
 import { mainFont } from "@/lib/fonts";
 import { WrapperProps } from "@/types";
 import "@/styles/globals.css";
-import ThemeProvider from "@/components/layout/ThemeProvider";
 import QueryProvider from "@/components/layout/QueryProvider";
 import { Toaster } from "@/components/ui/sonner";
 import LoadingOverlay from "@/components/LoadingOverlay";
@@ -12,14 +11,12 @@ export { metadata } from "./metadata";
 const RootLayout = ({ children }: WrapperProps) => {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${mainFont.className} antialiased`}>
+      <body className={`${mainFont.className} antialiased `}>
         <QueryProvider>
-          <ThemeProvider attribute="class" enableSystem>
             <Navbar />
             <main>{children}</main>
             <Toaster />
-            <LoadingOverlay />
-          </ThemeProvider>
+          <LoadingOverlay />
         </QueryProvider>
       </body>
     </html>
